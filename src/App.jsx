13e55d801e920f77299bba64638495e5646efe07
Evanny254 <evancherry254@gmail.com>
2023-10-31 22:39:07 +0300

    Completed Assignment

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,72 @@
+import { useEffect, useState } from 'react';
+import Stack from 'react-bootstrap/Stack';
 
-import { useState } from "react"
-import UserForm from "./Components/UserForm"
+import UserForm from './Components/UserForm';
+import UserList from './Components/UserList';
 
+const BASE_URL = 'http://localhost:3000/users';
 
 function App() {
-  const [Users,SetUsers] = useState([])
+	// useState hook allows us to work with dynamic data inside a component
+	const [users, setUsers] = useState([]);
+	const [input, setInput] = useState('');
 
-  const handleSaveUser =(user) => {
-    console.log(user)
-  };
-const name = 'Evans Kurgat';
-  return (
-    <div className= "Container mt5">
-      <UserForm handlesaveUser= {handleSaveUser} name={name} />
-       </div>
-  );
+	// save user to db.json
+	const handleSaveUser = (user) => {
+		fetch(BASE_URL, {
+			method: 'POST',
+			headers: {
+				'Content-Type': 'application/json',
+			},
+			body: JSON.stringify(user),
+		})
+			.then((res) => res.json())
+			.then((data) => setUsers([...users, data]))
+			.catch((err) => console.log(err));
+	};
+
+	/**
+	 * useEffect allows us to add extra effects to a component i.e
+	 * 1. Fetch data from an api
+	 * 2. Manually manipulating the DOM
+	 */
+	// 1. Runs every time the state/props change
+	// useEffect(() => {
+	// 	console.log('Can be executed');
+	// });
+
+	//2. Executed only once when the component is rendered
+	// useEffect(() => {
+	// 	console.log('Running');
+	// }, []);
+
+	// 3. Runs after the component renders and every time the dependancy changes
+	// useEffect(() => {
+	// 	console.log('Input: ', input);
+	// }, [input]);
+
+	// Fetch users from db.json
+	useEffect(() => {
+		fetch(BASE_URL)
+			.then((res) => res.json())
+			// once we get the data from db.json we store inside the state
+			.then((users) => setUsers(users))
+			.catch((err) => console.log(err));
+	}, []);
+
+	return (
+		<div className="container mt-5">
+			<Stack gap={3}>
+				<UserForm handleSaveUser={handleSaveUser} />
+
+				<hr />
+
+				<UserList users={users} />
+			</Stack>
+
+			{/* <input type="text" onChange={(e) => setInput(e.target.value)} /> */}
+		</div>
+	);
 }
 
-export default App
+export default App;
--- src/Components/UserForm.jsx
+++ src/Components/UserForm.jsx
@@ -1,35 +1,76 @@
-// import React from 'react';
+/**
+ * Must start with a capital letter
+ * Must return one parent element
+ * Use single responsibility principle
+ */
+import { useState } from 'react';
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
 import Stack from 'react-bootstrap/Stack';
 
-function UserForm(props) {
-  const { firstName, handleSaveUser } = props;
+// props desctructuring
+const UserForm = (props) => {
+	const { handleSaveUser } = props;
 
-  const handleClick = () => {
-    console.log('Clicked');
-  };
+	const user = {
+		firstName: '',
+		lastName: '',
+		age: '',
+	};
 
-  return (
-    <Stack direction="horizontal" gap={3}>
-      <Form.Control
-        className="me-auto"
-        placeholder="Add your item here..."
-      />
-      <Form.Control
-        className="me-auto"
-        placeholder="Add your item here..."
-      />
-      <Button
-        variant="secondary"
-        onClick={handleClick}
-      >
-        Submit
-      </Button>
-      <div className="vr" />
-      <Button variant="outline-danger">Reset</Button>
-    </Stack>
-  );
-}
+	const [formData, setFormData] = useState(user);
+
+	const handleFormData = (e) => {
+		setFormData({ ...formData, [e.target.name]: e.target.value });
+	};
+
+	const handleSaveForm = (e) => {
+		// prevents the default form behaviour of reloading
+		e.preventDefault();
+		// pass the user object as an argument back to parent component
+		handleSaveUser(formData);
+		// reset the entire form
+		setFormData(user);
+	};
+
+	return (
+		<form onSubmit={handleSaveForm}>
+			<Stack direction="horizontal" gap={3}>
+				<Form.Control
+					className="me-auto"
+					placeholder="First name"
+					name="firstName"
+					required
+					value={formData['firstName']}
+					onChange={handleFormData}
+				/>
+				<Form.Control
+					className="me-auto"
+					placeholder="Last name"
+					name="lastName"
+					required
+					value={formData['lastName']}
+					onChange={handleFormData}
+				/>
+				<Form.Control
+					className="me-auto"
+					placeholder="Age"
+					name="age"
+					type="number"
+					required
+					value={formData['age']}
+					onChange={handleFormData}
+				/>
+				<Button variant="secondary" type="submit">
+					Submit
+				</Button>
+				<div className="vr" />
+				<Button variant="outline-danger" type="reset">
+					Reset
+				</Button>
+			</Stack>
+		</form>
+	);
+};
 
 export default UserForm;
